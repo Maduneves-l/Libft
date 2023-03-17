279a45ea68a6f78e30e33bdd94010fb166fab0d8
Madu <127690148+Maduneves-l@users.noreply.github.com>
2023-03-17 16:19:04 +0000

    Add files via upload

---
+++ ft_strnstr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mneves-l <mneves-l@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/10 21:11:00 by mneves-l          #+#    #+#             */
+/*   Updated: 2023/03/10 21:11:02 by mneves-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (*needle == '\0')
+		return ((char *)haystack);
+	if (!needle)
+		return (NULL);
+	while (haystack[i] && i < len)
+	{
+		j = 0;
+		while (haystack[i + j] == needle[j] && needle[j] && i + j < len
+			&& haystack[i + j])
+			j++;
+		if (!needle[j])
+			return ((char *)haystack + i);
+		i++;
+	}
+	return (NULL);
+}
